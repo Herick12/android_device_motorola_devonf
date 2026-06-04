49f872cfb96566605955c907a706873998c2e1ae
Herick12 <66499756+Herick12@users.noreply.github.com>
2026-06-04 17:06:59 -0300

    Create omni_devonf.mk

---
+++ omni_devonf.mk
@@ -0,0 +1,7 @@
+$(call inherit-product, device/motorola/devonf/device.mk)
+
+PRODUCT_NAME := omni_devonf
+PRODUCT_DEVICE := devonf
+PRODUCT_BRAND := motorola
+PRODUCT_MODEL := Moto G73
+PRODUCT_MANUFACTURER := motorola
