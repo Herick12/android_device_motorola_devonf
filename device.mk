d369232f1691abec254aa5029ec18d6d85614ef8
Herick12 <66499756+Herick12@users.noreply.github.com>
2026-06-04 17:16:28 -0300

    Update device.mk

--- device.mk
+++ device.mk
@@ -1,7 +1,7 @@
 PRODUCT_DEVICE := devonf
 PRODUCT_NAME := omni_devonf
 PRODUCT_BRAND := motorola
-PRODUCT_MODEL := Motorola Device
+PRODUCT_MODEL := Moto G73
 PRODUCT_MANUFACTURER := motorola
 
 $(call inherit-product, $(SRC_TARGET_DIR)/product/base.mk)
