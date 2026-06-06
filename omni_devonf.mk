5eaa9cf3abe625185f38b5b6cb37d2d4246dc812
Herick12 <66499756+Herick12@users.noreply.github.com>
2026-06-05 22:47:00 -0300

    Update omni_devonf.mk

--- omni_devonf.mk
+++ omni_devonf.mk
@@ -1,3 +1,4 @@
+$(call inherit-product, vendor/twrp/config/common.mk)
 $(call inherit-product, device/motorola/devonf/device.mk)
 
 PRODUCT_NAME := omni_devonf
@@ -5,3 +6,18 @@ PRODUCT_DEVICE := devonf
 PRODUCT_BRAND := motorola
 PRODUCT_MODEL := Moto G73
 PRODUCT_MANUFACTURER := motorola
+
+RECOVERY_VARIANT := twrp
+
+TW_DEVICE_VERSION := devonf
+TW_THEME := portrait_hdpi
+
+TW_INCLUDE_CRYPTO := true
+TW_INCLUDE_NTFS := true
+TW_INCLUDE_LPTOOLS := true
+
+TW_HAS_MTP := true
+TW_INCLUDE_FASTBOOTD := true
+TW_EXCLUDE_DEFAULT_USB_INIT := true
+
+TW_DEFAULT_LANGUAGE := pt_BR
