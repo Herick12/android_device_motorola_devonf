ac1cc9366184ad20bc63286bd63a8d5548c46b82
Herick12 <66499756+Herick12@users.noreply.github.com>
2026-06-10 21:46:49 -0300

    Update device.mk

--- device.mk
+++ device.mk
@@ -1,8 +1,7 @@
 $(call inherit-product, vendor/twrp/config/common.mk)
-$(call inherit-product, device/motorola/devonf/device.mk)
 
-PRODUCT_DEVICE := devonf
 PRODUCT_NAME := omni_devonf
+PRODUCT_DEVICE := devonf
 PRODUCT_BRAND := motorola
 PRODUCT_MODEL := Moto G73
 PRODUCT_MANUFACTURER := motorola
@@ -11,6 +10,7 @@ PRODUCT_PACKAGES += \
     twrp \
     recovery \
     libtar \
-    toolbox
+    toolbox \
+    otatools
 
 RECOVERY_VARIANT := twrp
